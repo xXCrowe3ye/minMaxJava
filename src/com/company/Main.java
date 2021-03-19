8d4ffdb1a6ae437f66eb1eec87627b1af354665a
xXCrowe3ye <luffy1356@gmail.com>
2021-03-20 01:25:19 +0800

    initial release

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+//        boolean first = true;
+
+        while (true) {
+
+            System.out.println ("Enter number:");
+            boolean isAnInt = scanner.hasNextInt ();
+
+            if (isAnInt) {
+
+                int number = scanner.nextInt ();
+
+//
+
+                if (number > max) {
+                    max = number;
+                }
+
+                if (number < min) {
+                    min = number;
+                }
+
+            }
+            else {
+                break;
+            }
+
+            scanner.nextLine ();
+        }
+
+        System.out.println ("min= " + min + ", max= " + max);
+        scanner.close ();
+
+
+
+    }
+
+
+
+
+}
+
